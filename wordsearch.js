0fbc180dc429f4304afd40f36f8e0bb767ee068d
isaac-ad <isaacadnanu@gmail.com>
2023-02-17 09:30:03 -0500

    modified wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    for (const size of letters) {
+     const vertically = size.join("")
+     if(vertically.includes(word))
+     return true;
+    }
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    return false;
 }
 
 module.exports = wordSearch
+
+
